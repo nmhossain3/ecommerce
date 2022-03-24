53874e0a886e97ef5273834c6e6ceac44d442b8e
nmhossain3 <101846138+nmhossain3@users.noreply.github.com>
2022-03-23 22:09:53 -0400

    Add files via upload

---
+++ CS355eCommerce/README.md
@@ -0,0 +1,8 @@
+# eCommerce
+
+
+This was a very fun project for me to work on that was very different from
+the problem solving and logic software based applications I created. I wanted
+to play around and learn how to use front-end languages, such as HTML and CSS,
+so I emulated a mock template of an eCommerce website that has different 
+athletic gear and equipment similar to real world online shopping websites.
---
+++ CS355eCommerce/account.js
@@ -0,0 +1,22 @@
+
+
+  var LoginForm = document.getElementById("LoginForm");
+  var RegForm = document.getElementById("RegForm");
+  var Indicator = document.getElementById("Indicator");
+
+
+  function register(){
+    RegForm.style.transform = "translateX(0px)";
+    Login.style.transform = "translateX(0px)";
+    Indicator.style.transform = "translateX(100px)";
+
+  }
+
+  function login(){
+    RegForm.style.transform = "translateX(300px)";
+    Login.style.transform = "translateX(300px)";
+    Indicator.style.transform = "translateX(0px)";
+
+
+  }
+
